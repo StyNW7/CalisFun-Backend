e7981808f671cea71ae335259784024ed0832138
xturus138 <radityaramadhan.138@gmail.com>
2025-08-21 10:14:20 +0700

    fix: logout function

--- Backend/middleware/protect.js
+++ Backend/middleware/protect.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import TokenBlacklist from "../models/tokenblacklist.model.js";
 
-export const protect = (req, res, next) => {
+export const protect = async (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
   if (!token) {
@@ -8,6 +9,12 @@ export const protect = (req, res, next) => {
   }
 
   try {
+    const blacklistedToken = await TokenBlacklist.findOne({ token: token });
+    if (blacklistedToken) {
+      return res
+        .status(401)
+        .json({ message: "Token is blacklisted. Please log in again." });
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
---
+++ Backend/models/tokenblacklist.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const tokenBlacklistSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: "24h",
+  },
+});
+
+const TokenBlacklist = mongoose.model("TokenBlacklist", tokenBlacklistSchema);
+
+export default TokenBlacklist;
